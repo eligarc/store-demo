f7071728b86f4589b327684c5946b29499fb737a
eligarc <elioenaig041294@gmail.com>
2024-02-28 10:55:51 -0600

    Se agrega funcionalidad de vistas anidada - Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,26 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
+import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: ListComponent
-    },
-    {
+        component: ListComponent,
+      },
+      {
         path: 'about',
-        component: AboutComponent
-    },
+        component: AboutComponent,
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+      },
+    ],
+  },
 ];
--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkWithHref, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
