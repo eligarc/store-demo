67337b48ceab0ffe324daaa340092601a475c3c8
eligarc <elioenaig041294@gmail.com>
2024-02-27 23:48:02 -0600

    Agrega directiva para cambiar el color de fondo

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { CounterComponent } from '@shared/components/counter/counter.component';
 import { CommonModule } from '@angular/common';
 import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
+import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, CounterComponent, WaveAudioComponent],
+  imports: [CommonModule, CounterComponent, WaveAudioComponent, HighlightDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
