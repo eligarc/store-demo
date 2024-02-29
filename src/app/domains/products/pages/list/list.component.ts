f75a8f78415fd1c98a36a80ec52be863e5224c84
eligarc <elioenaig041294@gmail.com>
2024-02-28 23:25:26 -0600

    Se agrega listado de categorias

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,8 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -15,22 +17,37 @@ import { ProductService } from '@shared/services/product.service';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   // cart = signal<Product[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
-    this.productService.getProducts()
-    .subscribe({
-      next: (prods) => {
-        this.products.set(prods)
-      },
-      error: () => {}
-    })
+    this.getProducts();
+    this.getCategories();
   }
 
   addToCart(product: Product) {
     // this.cart.update(prevState => [...prevState, product]);
     this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
+    this.productService.getProducts().subscribe({
+      next: (prods) => {
+        this.products.set(prods);
+      },
+      error: () => {},
+    });
+  }
+
+  private getCategories() {
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: () => {},
+    });
   }
 }
