b4f16a2ce7be35720e782b805c5d08d3f131d2e4
eligarc <elioenaig041294@gmail.com>
2024-03-01 09:48:13 -0600

    Remove spaces in code

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -18,6 +18,7 @@ export class ProductComponent {
   // @Input({ required: true }) img: string = '';
   // @Input({ required: true }) price: number = 0;
   // @Input({ required: true }) title: string = '';
+
   @Input({ required: true }) product!: Product;
 
   @Output() addToCart = new EventEmitter();
